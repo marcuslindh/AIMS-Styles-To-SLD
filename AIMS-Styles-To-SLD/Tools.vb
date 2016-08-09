7bbcc97e410ff4e2bfa4111436b2afeb60802cf4
marcus lindh <marcusl1@hotmail.com>
2016-08-09 16:38:53 +0200

    deleted function not used

--- AIMS-Styles-To-SLD/Tools.vb
+++ AIMS-Styles-To-SLD/Tools.vb
@@ -13,55 +13,6 @@ Public Class Tools
         Request.Headers("Authorization") = "Basic " & AuthInfo
     End Sub
 
-    Public Shared Function IfElementToString(obj As XElement) As String
-        If obj IsNot Nothing Then
-            Return obj.Value
-        End If
-        Return ""
-    End Function
-
-    Public Shared Function IfElementToBoolean(obj As XElement) As Boolean
-        If obj IsNot Nothing Then
-            If obj.Value = "true" Then
-                Return True
-            End If
-        End If
-        Return False
-    End Function
-
-    Public Shared Function IfElementToLong(obj As XElement) As Long
-        If obj IsNot Nothing Then
-            If Not String.IsNullOrEmpty(obj.Value) Then
-                Return obj.Value
-            End If
-        End If
-        Return 0
-    End Function
-
-    Public Shared Function IfElementToInteger(obj As XElement) As Integer
-        If obj IsNot Nothing Then
-            If Not String.IsNullOrEmpty(obj.Value) Then
-                Return obj.Value
-            End If
-        End If
-        Return -1
-    End Function
-
-    Public Shared Function FormatSize(s As Long) As String
-        If s < 1024 ^ 2 Then
-            Return Math.Round((s / 1024), 1) & " KB"
-        ElseIf s < 1024 ^ 3 Then
-            Return Math.Round((s / 1024 ^ 2), 1) & " MB"
-        ElseIf s > 1024 ^ 3 Then
-            Return Math.Round((s / 1024 ^ 3), 1) & " GB"
-        End If
-    End Function
-
-    Public Shared Function StringToLong(str As String) As Long
-        Dim res As Long = 0
-        Long.TryParse(str, res)
-        Return res
-    End Function
 
 
 End Class
