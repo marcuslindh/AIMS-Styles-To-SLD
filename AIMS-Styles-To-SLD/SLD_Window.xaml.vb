dff72c086106a176b0780dc918d960125d6a1a8d
marcus lindh <marcusl1@hotmail.com>
2016-09-07 16:39:33 +0200

    added Show Log button

--- AIMS-Styles-To-SLD/SLD_Window.xaml.vb
+++ AIMS-Styles-To-SLD/SLD_Window.xaml.vb
@@ -5,4 +5,13 @@
     Private Sub SLD_Window_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
         SLD.Text = My.Computer.FileSystem.ReadAllText(FileName)
     End Sub
+
+    Private Sub ShowLog_Click(sender As Object, e As RoutedEventArgs)
+        Dim FileName As String = Environment.CurrentDirectory & "\SLD_Log.txt"
+        If IO.File.Exists(FileName) Then
+            System.Diagnostics.Process.Start(FileName)
+        Else
+            MessageBox.Show("There is no log file!", "", MessageBoxButton.OK, MessageBoxImage.Error)
+        End If
+    End Sub
 End Class
